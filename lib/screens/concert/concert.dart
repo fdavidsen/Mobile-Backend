06a67c6a2d71d518a513c985c3611133705717f0
fdavidsen <f.davidsen19@gmail.com>
2023-10-21 20:56:22 +0700

    Add search bar and filter chip on concert page

--- lib/screens/concert/concert.dart
+++ lib/screens/concert/concert.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:apple_todo/utilities/constants.dart';
 import 'package:apple_todo/providers/todo_provider.dart';
 import 'package:apple_todo/models/concert_model.dart';
 import 'package:apple_todo/screens/concert/concert_detail.dart';
@@ -16,12 +17,22 @@ class MyConcert extends StatefulWidget {
 }
 
 class _MyConcertState extends State<MyConcert> {
-  final String _apiUrl = 'https://raw.githubusercontent.com/fdavidsen/Mobile-Backend/master/lib/events.json';
   List? concertList;
   List? filteredList;
 
+  List<String> options = [
+    'No Filter',
+    'Live Music Venue',
+    'Concert Hall',
+    'Arena',
+  ];
+  String selectedOption = 'no_filter';
+  String searchQuery = '';
+
+  final _searchController = TextEditingController();
+
   Future<List?> getConcertData() async {
-    http.Response result = await http.get(Uri.parse(_apiUrl));
+    http.Response result = await http.get(Uri.parse(apiUrl));
 
     if (result.statusCode == HttpStatus.ok) {
       var responseBody = json.decode(result.body);
@@ -29,6 +40,17 @@ class _MyConcertState extends State<MyConcert> {
       return data.map((e) => Concert.fromJson(e)).toList();
     }
     return null;
+  }
+
+  void filterResults(String query) {
+    searchQuery = query;
+    List filterResults = concertList!
+        .where((item) =>
+            item.name.toLowerCase().contains(query.toLowerCase()) && (selectedOption != 'No Filter' ? item.subtype.contains(selectedOption) : true))
+        .toList();
+    setState(() {
+      filteredList = filterResults;
+    });
   }
 
   Future<void> loadData() async {
@@ -47,36 +69,79 @@ class _MyConcertState extends State<MyConcert> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 12, right: 12, top: 10, bottom: 50),
-      child: ListView.builder(
-          itemCount: filteredList?.length == null ? 0 : filteredList?.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 12),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: const EdgeInsets.only(top: 15, bottom: 18),
-                  color: context.watch<TodoProvider>().isDark ? const Color(0xff0e0e0e) : Colors.white,
-                  child: ListTile(
-                    onTap: () {
-                      MaterialPageRoute route = MaterialPageRoute(builder: (_) => ConcertDetail(concert: filteredList![index]));
-                      Navigator.push(context, route);
+      padding: const EdgeInsets.only(left: 12, right: 12, top: 15, bottom: 50),
+      child: Column(
+        children: [
+          TextField(
+            controller: _searchController,
+            onChanged: filterResults,
+            decoration: InputDecoration(
+              hintText: "Cari nama konser",
+              prefixIcon: const Icon(Icons.search),
+              hintStyle: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.blue : const Color(0xff1e1e1e)),
+            ),
+            style: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.blue : const Color(0xff1e1e1e)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 30),
+            child: Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                children: options.map((option) {
+                  return ChoiceChip(
+                    label: Text(
+                      option,
+                      style: TextStyle(color: selectedOption == option ? Colors.white : Colors.blue),
+                    ),
+                    selected: selectedOption == option,
+                    selectedColor: Colors.blue,
+                    backgroundColor: context.watch<TodoProvider>().isDark ? const Color(0xff1a1a1a) : Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    onSelected: (selected) {
+                      setState(() {
+                        selectedOption = selected ? option : '';
+                        filterResults(searchQuery);
+                      });
                     },
-                    leading: CircleAvatar(backgroundImage: NetworkImage(filteredList![index].thumbnail)),
-                    title: Text(
-                      filteredList![index].name,
-                      style: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.white : Colors.black),
+                  );
+                }).toList()),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: filteredList?.length == null ? 0 : filteredList?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 15, bottom: 18),
+                        color: context.watch<TodoProvider>().isDark ? const Color(0xff0e0e0e) : Colors.white,
+                        child: ListTile(
+                          onTap: () {
+                            MaterialPageRoute route = MaterialPageRoute(builder: (_) => ConcertDetail(concert: filteredList![index]));
+                            Navigator.push(context, route);
+                          },
+                          leading: CircleAvatar(backgroundImage: NetworkImage(filteredList![index].thumbnail)),
+                          title: Text(
+                            filteredList![index].name,
+                            style: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.white : Colors.black),
+                          ),
+                          subtitle: Text(
+                            'Subtype: ${filteredList![index].subtype} - Rating: ${filteredList![index].rating}',
+                            style: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.white : Colors.black),
+                          ),
+                        ),
+                      ),
                     ),
-                    subtitle: Text(
-                      'Subtype: ${filteredList![index].subtype} - Rating: ${filteredList![index].rating}',
-                      style: TextStyle(color: context.watch<TodoProvider>().isDark ? Colors.white : Colors.black),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,5 +2,7 @@ const String sharedPreferencesKeyDarkMode = 'apple-dark_mode';
 
 const String datetimeFormat = 'dd MMM yyyy';
 
+const String apiUrl = 'https://raw.githubusercontent.com/fdavidsen/Mobile-Backend/master/lib/events.json';
+
 const String defaultConcertThumbnail =
     'https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D';
